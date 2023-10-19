4159af9006f82b1ebbc2d8360fca80075163934d
Pratibhapa <pratibhaa4321@gmail.com>
2023-10-19 08:43:43 +0545

    first commmit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Webcam from 'react-webcam';
 
-function App() {
+const WebcamCapture = () => {
+  const webcamRef = React.useRef(null);
+  const  [imgsrc, setImgsrc] = React.useState(null);
+
+
+  const capture = React.useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImgsrc(imageSrc);
+  }, [webcamRef, setImgsrc]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <>
+      <Webcam
+        muted={false}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+      />
+      <button onClick={capture}>Capture photo</button>
+      {imgsrc && (
+        <img
+          src={imgsrc}
+        />
+      )}
 
-export default App;
+    </>
+  );
+};
+
+export default WebcamCapture;
